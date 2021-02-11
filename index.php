08356e745d46c286c9d36b2a04d7245702aa5ef0
Alessandro Rossi <alessandro.rossi7610@gmail.com>
2021-02-11 15:36:03 +0100

    Added login navigation

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
-<?php 
- 
-require "controllers/controller.php"; 
- 
- 
-session_start(); 
-if (isset($_GET['action'])) { 
-  $action = $_GET['action']; 
-  switch ($action) { 
-    case 'home': 
-      displayHome(); 
-      break; 
- 
-    default: 
-      displayHome(); 
-      break; 
-  } 
-}else { 
-  displayHome(); 
-} 
+<?php
+
+require "controllers/controller.php";
+
+
+session_start();
+if (isset($_GET['action'])) {
+  $action = $_GET['action'];
+  switch ($action) {
+    case 'home':
+      displayHome();
+      break;
+    case 'login':
+      if (isset($_POST)) {
+        login($_POST);
+      }else {
+        displayLogin();
+      }
+      break;
+
+    default:
+      displayHome();
+      break;
+  }
+}else {
+  displayHome();
+}
