4de5e2e0035a29248ca71c43d02d562e6e16c69f
Alessandro Rossi <alessandro.rossi7610@gmail.com>
2021-02-11 15:32:44 +0100

    Added is login correct function

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,5 +1,5 @@
 <?php
-
+require "models/login.php";
 
 function displayHome()
 {
@@ -7,4 +7,16 @@ function displayHome()
 }
 function displayLogin(){
   require "views/login.php";
+}
+
+/**
+ *
+ * @param type array inputUserEmail and inputUserPassword
+ */
+function login($userLoginData){
+  if(isset($userLoginData['inputUserEmail']) && isset($userLoginData['inputUserPassword'])){
+    isLoginCorrect($userLoginData);
+  }else {
+    displayLogin();
+  }
 }
