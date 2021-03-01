5b33382afbd5b29db8ebfaa273324c146926da9c
Alessandro Rossi <alessandro.rossi7610@gmail.com>
2021-03-01 11:30:18 +0100

    Added card for the image in concours

--- views/concoursLogged.php
+++ views/concoursLogged.php
@@ -7,12 +7,23 @@ $title = "Concours";
   <h1>Concours</h1>
 </div>
 <div class="concours-container">
-  <div class="concours-currentLevel">
-    <span><h3>Photo <?php echo $_SESSION['currentLevel'] ?>/10</h3></span>
+
+
+
+  <div class="card">
+    <div class="card-header">
+      <div class="concours-currentLevel">
+        <span><h3>Photo <?php echo $_SESSION['currentLevel'] ?>/10</h3></span>
+      </div>
+    </div>
+    <div class="card-sep">
+      <hr class="card-separator">
+    </div>
+    <div class="concours-imageContainer">
+      <img class="concours-image"src="<?php echo $_SESSION['pathToImage'] ?>" alt="">
+    </div>
   </div>
-  <div class="concours-imageContainer">
-    <img class="concours-image"src="<?php echo $_SESSION['pathToImage'] ?>" alt="">
-  </div>
+
   <form class="concours-form" action="index.php?action=concours" method="post">
 
     <div class="concours-formGroup">
@@ -23,19 +34,25 @@ $title = "Concours";
       <label class="concours-formLabel" for="userInputLongitude">Longitude (Y.yyyyy)</label>
       <input class="concours-formInput" type="text" name="userInputLongitude" value="" required>
     </div>
+      <?php if ($_SESSION['attempsNumber'] == 0): ?>
 
-    <div class="concours-tryScores">
-      <?php if ($_SESSION['attempsNumber'] == 1): ?>
-        <span class="concours-buttonLabel">Tentative 1 : <?php echo $_SESSION['tryScores']['Try1'] ?></span>
+      <?php elseif ($_SESSION['attempsNumber'] == 1): ?>
+        <div class="concours-tryScores">
+          <span class="concours-buttonLabel">Tentative 1 : <?php echo $_SESSION['tryScores']['Try1'] ?></span>
+        </div>
+
       <?php elseif($_SESSION['attempsNumber'] == 2): ?>
-        <span class="concours-buttonLabel">Tentative 1 : <?php echo $_SESSION['tryScores']['Try1'] ?></span>
-        <span class="concours-buttonLabel">Tentative 2 : <?php echo $_SESSION['tryScores']['Try2'] ?></span>
+        <div class="concours-tryScores">
+          <span class="concours-buttonLabel">Tentative 1 : <?php echo $_SESSION['tryScores']['Try1'] ?></span>
+          <span class="concours-buttonLabel">Tentative 2 : <?php echo $_SESSION['tryScores']['Try2'] ?></span>
+        </div>
       <?php elseif($_SESSION['attempsNumber'] == 3): ?>
-        <span class="concours-buttonLabel">Tentative 1 : <?php echo $_SESSION['tryScores']['Try1'] ?></span>
-        <span class="concours-buttonLabel">Tentative 2 : <?php echo $_SESSION['tryScores']['Try2'] ?></span>
-        <span class="concours-buttonLabel">Tentative 3 : <?php echo $_SESSION['tryScores']['Try3'] ?></span>
+        <div class="concours-tryScores">
+          <span class="concours-buttonLabel">Tentative 1 : <?php echo $_SESSION['tryScores']['Try1'] ?></span>
+          <span class="concours-buttonLabel">Tentative 2 : <?php echo $_SESSION['tryScores']['Try2'] ?></span>
+          <span class="concours-buttonLabel">Tentative 3 : <?php echo $_SESSION['tryScores']['Try3'] ?></span>
+        </div>
       <?php endif; ?>
-    </div>
     <div class="concours-formGroup">
       <label class="concours-buttonLabel" for="btnTry">Tentative <?php echo $_SESSION['attempsNumber'] ?>/3</label>
       <button class="secondaryButton <?php if ($_SESSION['attempsNumber']>=3){echo 'disabled';} ?>" type="submit" name="btnTry"<?php if ($_SESSION['attempsNumber']>=3){echo 'disabled';} ?>>Vérifier</button>
