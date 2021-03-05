6468006ca2b100124e77e0a6ca7583cc18ba2a85
Alessandro Rossi <alessandro.rossi7610@gmail.com>
2021-03-05 09:34:21 +0100

    Now only used css are loaded

--- views/layout.php
+++ views/layout.php
@@ -8,19 +8,26 @@
     <link rel="icon" type="image/png" href="content\icones\favicon.png" sizes="16x16">
 
     <link rel="stylesheet" href="content/styles/main.css">
-    <link rel="stylesheet" href="content/styles/navbar.css">
     <link rel="stylesheet" href="content/styles/color.css">
-    <link rel="stylesheet" href="content/styles/login.css">
-    <link rel="stylesheet" href="content/styles/register.css">
-    <link rel="stylesheet" href="content/styles/concours.css">
-    <link rel="stylesheet" href="content/styles/home.css">
+    <link rel="stylesheet" href="content/styles/navbar.css">
     <link rel="stylesheet" href="content/styles/sidebar.css">
-    <link rel="stylesheet" href="content/styles/cards.css">
-    <link rel="stylesheet" href="content/styles/leaderboard.css">
-    <link rel="stylesheet" href="content/styles/profile.css">
+    <?php if ($title=='Accueil'): ?>
+      <link rel="stylesheet" href="content/styles/home.css">
+    <?php elseif($title=='Login'): ?>
+      <link rel="stylesheet" href="content/styles/login.css">
+    <?php elseif($title=='Register'): ?>
+      <link rel="stylesheet" href="content/styles/register.css">
+    <?php elseif($title=='Concours'): ?>
+      <link rel="stylesheet" href="content/styles/concours.css">
+      <link rel="stylesheet" href="content/styles/cards.css">
+    <?php elseif($title=='Classement'): ?>
+      <link rel="stylesheet" href="content/styles/leaderboard.css">
+    <?php elseif($title=='Profile'): ?>
+      <link rel="stylesheet" href="content/styles/profile.css">
+      <link rel="stylesheet" href="content/styles/cards.css">
+    <?php endif; ?>
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="content\scripts\theonlyone.js" charset="utf-8"></script>
   </head>
@@ -78,7 +85,7 @@
                 <button type="button" class="sidebar-quit "><span class="material-icons md-36">clear</span></button>
                 <a href="index.php?action=home">Accueil</a>
                 <a href="index.php?action=concours">Concours</a>
-                <a href="index.php?action=classement">Classement</a>
+                <a href="index.php?action=leaderboard">Classement</a>
                 <?php if (isset($_SESSION['isLogged'])): ?>
                   <a href="index.php?action=logout">Déconnexion</a>
                 <?php endif; ?>
@@ -89,6 +96,15 @@
                 <?php endif; ?>
             </div>
       </div>
+        <?php if (isset($error)): ?>
+          <div class="errorContainer">
+          <?php foreach ($error as $errorID => $errorMessage): ?>
+            <div class="error">
+            <?php echo $errorMessage ?>
+            </div>
+          <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
     </header>
     <div class="container">
       <?php echo $content ?>
